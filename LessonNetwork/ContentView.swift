a94062a028cf4294e373534143fb22b7b96d5db1
metelykx <ivaschenkoodenys@gmail.com>
2025-07-02 14:45:37 +0300

    mainActor

--- LessonNetwork/ContentView.swift
+++ LessonNetwork/ContentView.swift
@@ -10,22 +10,18 @@ import SwiftUI
 struct ContentView: View {
     
     //MARK: - Proporties
-    @State var news: [Article] = []
+    @StateObject var vm = ViewModel()
     
     
     //MARK: - Body
     var body: some View {
         VStack {
            
-            ForEach(news, id: \.url) { article in
+            ForEach(vm.topNews, id: \.url) { article in
                 Text(article.title)
             }
         }
-        .onAppear {
-            Task {
-                await fetchNews()
-            }
-        }
+        
     }
     
     //MARK: - Methods
--- LessonNetwork/ViewModel.swift
+++ LessonNetwork/ViewModel.swift
@@ -7,21 +7,29 @@
 
 import Foundation
 
+@MainActor
 final class ViewModel: ObservableObject {
     
     //MARK: - Proporties
     @Published var topNews: [Article] = []
     
+    init() {
+        fetchNews()
+    }
+    
     //MARK: - Methods
     
-    func fetchNews() async {
-        do {
-            let articles = try await NetworkManager.shared.getNews()
-            topNews = articles.articles
-        } catch {
-            if let error = error as? NetworkError {
-                print(error)
+    func fetchNews() {
+        Task {
+            do {
+                let articles = try await NetworkManager.shared.getNews()
+                topNews = articles.articles
+            } catch {
+                if let error = error as? NetworkError {
+                    print(error)
+                }
             }
         }
+        
     }
 }
