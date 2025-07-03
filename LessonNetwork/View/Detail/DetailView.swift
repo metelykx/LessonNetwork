18130dce90b9658a20c43dafa9ab1bdff09f4103
metelykx <ivaschenkoodenys@gmail.com>
2025-07-03 12:56:15 +0300

    add scrollView

--- LessonNetwork/View/Detail/DetailView.swift
+++ LessonNetwork/View/Detail/DetailView.swift
@@ -13,17 +13,19 @@ struct DetailView: View {
     let article: Article
     
     var body: some View {
-        GeometryReader { reader in
-            if let url = article.urlToImage, let imageURL = URL(string: url) {
-                AsyncImage(url: imageURL) { phase in
-                    phase.image!
-                    .resizable()
-                    .offset(y: -reader.frame(in: .global).minY)
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: SizeConstant.screenWidth, height: reader.frame(in: .global).minY + SizeConstant.avatarHeight + 10)
+        ScrollView(.vertical, showsIndicators: false) {
+            GeometryReader { reader in
+                if let url = article.urlToImage, let imageURL = URL(string: url) {
+                    AsyncImage(url: imageURL) { phase in
+                        phase.image!
+                            .resizable()
+                            .offset(y: -reader.frame(in: .global).minY)
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: SizeConstant.screenWidth, height: reader.frame(in: .global).minY + SizeConstant.avatarHeight + 10)
+                    }
                 }
-            }
-        }.frame(height: UIScreen.main.bounds.width)
+            }.frame(height: UIScreen.main.bounds.width)
+        }
     }
 }
 
